c55b31b595300fc366b35667d1eba56faab72154
rahat <rahatalshehab@gmail.com>
2020-06-05 03:17:23 +0600

    services merge

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+import ipinfo
 
+# google maps
+from flask_googlemaps import GoogleMaps, Map
+from googleplaces import GooglePlaces, types, lang
 
-# Degining app
+# Begining app
 app = Flask(__name__)
 app.secret_key = "suidhfliasdfoagdrgdeocf"
+app.run(debug=True)
+
+MAPS_API_KEY = 'AIzaSyCskEcqqtB89CfG-jJXihqF20SZSlRxzFo'
+app.config['GOOGLEMAPS_KEY'] = MAPS_API_KEY
+GoogleMaps(app)
 
 
 # Initial Page
@@ -47,3 +56,94 @@ def your_url():
 
                 # If correct username and password
                 return render_template('placeholder.html')
+
+
+@app.route('/services')
+def services():
+    # list names of services (shown to user)
+    services = [
+               "Treatment",
+               "Find nearest clinic",
+               "Find nearest drugstore"
+               ]
+    # links of services
+    services_link = [
+                    "treatment",
+                    "nearest_clinic",
+                    "nearest_drugstore"
+                    ]
+
+
+    return render_template('services.html', services=services,
+                            services_link=services_link)
+
+
+@app.route('/nearest_clinic')
+def clinic():
+    # calls mapview() and displays "hospital"
+    items = mapview("hospital")
+    return render_template('nearest_clinic.html', map=items[0], name=items[1])
+
+
+@app.route('/nearest_drugstore')
+def drugstore():
+    # calls mapview() and displays "pharmacy"
+    items = mapview("pharmacy")
+    return render_template('nearest_drugstore.html', map=items[0], name=items[1])
+
+
+# detect location using IP
+def ip_coordinates():
+    ipinfo_token = '0562b9f1a0bc99'
+    handler = ipinfo.getHandler(ipinfo_token)
+    ip_data = handler.getDetails()
+    # city = data.city + data.region + data.country
+    latitude = ip_data.latitude
+    longitude = ip_data.longitude
+    return latitude, longitude
+
+
+def mapview(itemtype):
+    if itemtype == "hospital":
+        search_type = [types.TYPE_HOSPITAL]
+        pass
+    if itemtype == "pharmacy":
+        search_type = [types.TYPE_PHARMACY]
+        pass
+    
+    places = GooglePlaces(MAPS_API_KEY)
+    coordinates = ip_coordinates()
+    latitude = coordinates[0]
+    longitude = coordinates[1]
+
+    search_result = places.nearby_search(
+        lat_lng={'lat': latitude, 'lng': longitude},
+        radius=3000,
+        types=search_type
+    )
+
+    if search_result.has_attributions:
+        print(search_result.html_attributions)
+
+    markers = []
+    name = []
+    for place in search_result.places:
+        # place.get_details()
+        item_name = place.name
+        item_latitude = place.geo_location['lat']
+        item_longitude = place.geo_location['lng']
+        # item_address = place.formatted_address
+        location_of_item = { 'lat': str(item_latitude), 'lng': str(item_longitude), 'infobox': item_name}
+        markers.append(location_of_item)
+        name.append(item_name)
+
+    map_data = Map(
+        identifier="map",
+        lat=item_latitude,
+        lng=item_longitude,
+        markers=markers,
+        style="height:80%;width:100%;margin:auto;",
+        zoom=16
+    )
+
+    return map_data, name
